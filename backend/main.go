3e5053fc93a4bf34af1240073321f4ea5b78dcdb
balpa <berkealtiparmak@outlook.com>
2023-11-06 00:28:18 +0300

    FUCKING CORS

--- backend/main.go
+++ backend/main.go
@@ -1,19 +1,44 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
 
-func main() {
-	handler := http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
-		resp := []byte(`{"status": "ok"}`)
-		rw.Header().Set("Content-Type", "application/json")
-		rw.Header().Set("Content-Length", fmt.Sprint(len(resp)))
-		rw.Write(resp)
-	})
+type payload struct {
+	Name string `json:"name"`
+}
 
-	log.Println("Server is available at http://localhost:8000")
-	log.Fatal(http.ListenAndServe(":8000", handler))
+func welcome(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.Header().Set("Access-Control-Allow-Origin", "http://localhost:8000/api/welcome")
+	w.Header().Set("Access-Control-Max-Age", "10")
+
+	if r.Method != http.MethodPost {
+		http.Error(w, "Only POST requests are allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	var payload payload
+
+	decoder := json.NewDecoder(r.Body)
+
+	if err := decoder.Decode(&payload); err != nil {
+		http.Error(w, "Invalid request payload", http.StatusBadRequest)
+		return
+	}
+
+	fmt.Fprintf(w, "Welcome to the server, %s!", payload.Name)
+	fmt.Println("Endpoint Hit: welcome")
+}
+
+func handleRequests() {
+	http.HandleFunc("/api/welcome", welcome)
+	log.Fatal(http.ListenAndServe(":8000", nil))
+}
+
+func main() {
+	handleRequests()
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,33 @@
+'use client'
+
 import styles from './page.module.css'
+import React from 'react'
+import axios from 'axios'
 
 export default function Home() {
+  const [ name, setName ] = React.useState('')
+  const [ response, setResponse ] = React.useState('')
+
+  const sendRequest = () => {
+    if (name !== '') {
+      axios({
+        method: 'post',
+        url: 'http://localhost:8000/api/welcome',
+        data: { name: name },
+        headers: { 'Content-Type': 'application/json' }
+      })
+      .then((response) => setResponse(response))
+      .catch((error) => console.log(error));
+    }
+  }
+
   return (
     <main className={styles.main}>
-      HOOPLYTICS
+      <div style={{display:'flex', flexDirection: 'column', gap: '10px'}}>
+        <input onChange={(e) => setName(e.target.value)} placeholder='write your name'></input>
+        <button onClick={() => sendRequest()}>send request</button>
+        <div>response from the server: { response }</div>
+      </div>
     </main>
   )
 }
