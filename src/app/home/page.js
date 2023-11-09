0a5eb4134336268d6017bf06c44acc75c8f9af65
balpa <berkealtiparmak@outlook.com>
2023-11-10 00:01:37 +0300

    homepage and backend

---
+++ src/app/home/page.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './home.module.css'
+
+export default function Home() {
+    return (
+      <div className={styles.homepage_main_wrapper}>
+        
+      </div>
+    )
+  }
--- src/app/page.js
+++ src/app/page.js
@@ -2,32 +2,11 @@
 
 import styles from './page.module.css'
 import React from 'react'
-import axios from 'axios'
 
-export default function Home() {
-  const [ name, setName ] = React.useState('')
-  const [ response, setResponse ] = React.useState('')
-
-  const sendRequest = () => {
-    if (name !== '') {
-      axios({
-        method: 'post',
-        url: 'http://localhost:3001/api/welcomePost',
-        data: { name: name },
-        headers: { 'Content-Type': 'application/json' }
-      })
-      .then((response) => setResponse(response.data))
-      .catch((error) => console.log(error));
-    }
-  }
+export default function DeprecatedHome() {
 
   return (
     <main className={styles.main}>
-      <div style={{display:'flex', flexDirection: 'column', gap: '10px'}}>
-        <input onChange={(e) => setName(e.target.value)} placeholder='write your name'></input>
-        <button onClick={() => sendRequest()}>send request</button>
-        <div>response: { response }</div>
-      </div>
     </main>
   )
 }
