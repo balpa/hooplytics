01c8fc61b2c252bfbf84706c657cb709c66f1b16
balpa <berkealtiparmak@outlook.com>
2023-11-11 00:02:26 +0300

    need to create r server atm

--- backend/main.go
+++ backend/main.go
@@ -7,10 +7,46 @@ import (
 
 	"github.com/gorilla/mux"
 	"github.com/rs/cors"
+	"github.com/senseyeio/roger"
 )
 
 type payload struct {
-	Name string `json:"name"`
+	Name   string `json:"name"`
+	Script string `json:"script"`
+}
+
+func executeRCode(script string) string {
+	rClient, err := roger.NewRClient("127.0.0.1", 3002)
+	if err != nil {
+		fmt.Println("Failed to create R client: " + err.Error())
+		return "Failed to create R client"
+	}
+
+	output, err := rClient.Eval(script)
+	if err != nil {
+		fmt.Println("Command failed: " + err.Error())
+	}
+
+	return output.(string)
+}
+
+func executeR(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Only POST requests are allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	var payload payload
+
+	decoder := json.NewDecoder(r.Body)
+
+	if err := decoder.Decode(&payload); err != nil {
+		http.Error(w, "Invalid request payload", http.StatusBadRequest)
+		return
+	}
+
+	fmt.Fprintf(w, "Output: %s", executeRCode(payload.Script))
+	fmt.Println("Endpoint Hit: executeR")
 }
 
 func welcomeGet(w http.ResponseWriter, r *http.Request) {
@@ -56,6 +92,7 @@ func handleRequests() {
 
 	r.HandleFunc("/api/welcomeGet", welcomeGet)
 	r.HandleFunc("/api/welcomePost", welcomePost)
+	r.HandleFunc("/api/runRcode", executeR)
 
 	http.ListenAndServe(":3001", nil)
 }
--- src/app/home/page.js
+++ src/app/home/page.js
@@ -1,10 +1,24 @@
+"use client"
+
 import React from 'react'
 import styles from './home.module.css'
+import axios from 'axios';
 
 export default function Home() {
+    const [script, setScript] = React.useState('');
+    const [RCodeOutput, setRCodeOutput] = React.useState('');
+
+    const sendRequest = () => {
+      axios.post('http://localhost:3001/api/runRcode', { script: script })
+      .then((response) => setRCodeOutput(response.data))
+      .catch((error) => console.log(error));
+    }
+
     return (
       <div className={styles.homepage_main_wrapper}>
-        
+        <input onChange={(e) => {setScript(e.target.value)}} placeholder='R script here'></input>
+        <button onClick={() => {sendRequest()}}>send</button>
+        <div style={{color: 'black'}}>{ RCodeOutput }</div>
       </div>
     )
   }
